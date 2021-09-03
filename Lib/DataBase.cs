632ee3afba19cc3bd3b9e4a366a7aa0157f14c4d
Курицын Алексей <al.curitsin@gmail.com>
2021-09-04 00:17:10 +0500

    Add Lib

---
+++ Lib/DataBase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+namespace Lib
+{
+    public class DataBase
+    {
+        private const string MySQL_ini = @"..\..\..\..\..\..\MySQL.ini"; //  Ключь от MySql :)
+        private readonly MySqlConnection _db;
+        private MySqlCommand _command;
+
+        public DataBase()
+        {
+            using (FileStream fs = new FileStream(MySQL_ini, FileMode.Open))
+            {
+                byte[] array = new byte[fs.Length];
+                fs.Read(array, 0, array.Length);
+
+                string MySQL_db = System.Text.Encoding.Default.GetString(array);
+                _db = new MySqlConnection(MySQL_db);
+            }
+
+            _command = new MySqlCommand { Connection = _db };
+        }
+
+        public void Open()
+        {
+            _db.Open();
+        }
+
+        public void Close()
+        {
+            _db.Close();
+        }
+    }
+}
