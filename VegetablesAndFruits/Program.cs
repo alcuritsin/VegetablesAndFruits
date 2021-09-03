3bbe0b534c7ad0c62c14b702f1ce0c2e1bc59911
Курицын Алексей <al.curitsin@gmail.com>
2021-09-04 00:40:55 +0500

    Init

---
+++ VegetablesAndFruits/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using Lib;
+
+namespace VegetablesAndFruits
+{
+    class Program
+    {
+        static void Main()
+        {
+            var db = new DataBase();
+            
+        }
+    }
+}
