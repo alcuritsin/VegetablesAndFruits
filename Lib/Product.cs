58fb9c2c7a94a27088164f3140db1f664884860b
Курицын Алексей <al.curitsin@gmail.com>
2021-09-04 00:38:03 +0500

    Add Product

---
+++ Lib/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace Lib
+{
+    public class Product
+    {
+        public enum TypeProduct
+        {
+            Vegetable,
+            Fruit
+        }
+        public enum ColorProduct
+        {
+            Red, Green, Orange, Yellow, Purple
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public TypeProduct Type { get; set; }
+        public ColorProduct Color { get; set; }
+        public int EnergyValue { get; set; }
+        
+    }
+}
