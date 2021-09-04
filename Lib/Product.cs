5cd271189abdd98dad494b97282dba17a2ff8c8b
alcuritsin <al.curitsin@gmail.com>
2021-09-04 15:58:42 +0500

    Change Class Product Изменил изначальный enum на string

--- Lib/Product.cs
+++ Lib/Product.cs
@@ -2,20 +2,26 @@
 {
     public class Product
     {
-        public enum TypeProduct
-        {
-            Vegetable,
-            Fruit
-        }
-        public enum ColorProduct
-        {
-            Red, Green, Orange, Yellow, Purple
-        }
+        // Enum для SQL очень спорный вариант.
+        // Поэтому было принято решение от него отказаться.
+        // Но так как по условиям задания создаётся однотабличная БД,
+        // то тип и цвет будем хранить в виде текста.
+        // Хотя это и нарушает правило единообразия значений...
+        
+        // public enum TypeProduct
+        // {
+        //     Vegetable,
+        //     Fruit
+        // }
+        // public enum ColorProduct
+        // {
+        //     Red, Green, Orange, Yellow, Purple
+        // }
 
         public int Id { get; set; }
         public string Name { get; set; }
-        public TypeProduct Type { get; set; }
-        public ColorProduct Color { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
         public int EnergyValue { get; set; }
         
     }
---
+++ request.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE table_product
+(
+    id            INT AUTO_INCREMENT PRIMARY KEY,
+    name          VARCHAR(50) NOT NULL,
+    type_product  VARCHAR(50) NOT NULL,
+    color_product VARCHAR(50) NOT NULL,
+    energy_value  INT         NOT NULL
+);
+
+INSERT INTO table_product
+    (name, type_product, color_product, energy_value)
+    VALUE ('Капуста','Овощ','Зелёный','25');
+
+SELECT name, type_product, color_product, energy_value
+FROM table_product;
+
